5a4c4fcdf6d7bd8d565d4e782e20c2a8ed808a48
Suzan Stockey Pereira <suzan-sp@hotmail.com>
2025-10-30 14:26:37 -0300

    feat(project): Initial project structure via Spring Initializr

---
+++ src/main/java/io/github/suzanstockey/toolschalenge/ToolsChalengeApplication.java
@@ -0,0 +1,13 @@
+package io.github.suzanstockey.toolschalenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsChalengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsChalengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/suzanstockey/toolschalenge/ToolsChalengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.suzanstockey.toolschalenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsChalengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
