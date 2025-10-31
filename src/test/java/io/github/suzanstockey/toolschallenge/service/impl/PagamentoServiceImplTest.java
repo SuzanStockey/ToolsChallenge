7110aa0108e311583b0cd7c8a5af969d7eda8dde
Suzan Stockey Pereira <suzan-sp@hotmail.com>
2025-10-31 17:23:12 -0300

    test(service): Add unit tests for 'consultarPorId' and 'consultarTodos'

--- src/test/java/io/github/suzanstockey/toolschallenge/service/impl/PagamentoServiceImplTest.java
+++ src/test/java/io/github/suzanstockey/toolschallenge/service/impl/PagamentoServiceImplTest.java
@@ -1,6 +1,7 @@
 package io.github.suzanstockey.toolschallenge.service.impl;
 
 import io.github.suzanstockey.toolschallenge.exception.TransacaoJaExistenteException;
+import io.github.suzanstockey.toolschallenge.exception.TransacaoNaoEncontradaException;
 import io.github.suzanstockey.toolschallenge.model.StatusTransacao;
 import io.github.suzanstockey.toolschallenge.model.dto.request.PagamentoRequest;
 import io.github.suzanstockey.toolschallenge.model.dto.response.PagamentoResponse;
@@ -14,6 +15,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -79,6 +84,81 @@ class PagamentoServiceImplTest {
 
         assertTrue(exception.getMessage().contains(idDuplicado));
         verify(repository, never()).save(any(Transacao.class));
+        verify(mapper, never()).toResponse(any(Transacao.class));
+    }
+
+    @Test
+    @DisplayName("Deve consultar por ID com sucesso (caminho feliz)")
+    void deveConsultarPorIdComSucesso() {
+        final String idTeste = "id-existente-456";
+
+        var transacaoEntidade = mock(Transacao.class);
+        var response = mock(PagamentoResponse.class);
+
+        when(repository.findById(idTeste)).thenReturn(Optional.of(transacaoEntidade));
+
+        when(mapper.toResponse(transacaoEntidade)).thenReturn(response);
+
+        PagamentoResponse resultado = pagamentoService.consultarPorId(idTeste);
+
+        assertNotNull(resultado);
+
+        verify(repository).findById(idTeste);
+        verify(mapper).toResponse(transacaoEntidade);
+    }
+
+    @Test
+    @DisplayName("Deve falhar ao consultar por ID inexistente")
+    void deveFalharConsultarPorIdInexistente(){
+        final String idInexistente = "id-inexistente-789";
+
+        when(repository.findById(idInexistente)).thenReturn(Optional.empty());
+
+        TransacaoNaoEncontradaException exception = assertThrows(TransacaoNaoEncontradaException.class, () -> pagamentoService.consultarPorId(idInexistente));
+
+        assertTrue(exception.getMessage().contains(idInexistente));
+
+        verify(mapper, never()).toResponse(any(Transacao.class));
+    }
+
+    @Test
+    @DisplayName("Deve consultar todos os pagamentos com sucesso (caminho feliz)")
+    void deveConsultarTodosComSucesso() {
+        var transacao1 = mock(Transacao.class);
+        var transacao2 = mock(Transacao.class);
+        var response1 = mock(PagamentoResponse.class);
+        var response2 = mock(PagamentoResponse.class);
+
+        List<Transacao> transacoesEntidades = List.of(transacao1, transacao2);
+
+        when(repository.findAll()).thenReturn(transacoesEntidades);
+
+        when(mapper.toResponse(transacao1)).thenReturn(response1);
+        when(mapper.toResponse(transacao2)).thenReturn(response2);
+
+        List<PagamentoResponse> resultados = pagamentoService.consultarTodos();
+
+        assertNotNull(resultados);
+        assertEquals(2, resultados.size());
+        assertTrue(resultados.contains(response1));
+        assertTrue(resultados.contains(response2));
+
+        verify(repository).findAll();
+        verify(mapper, times(2)).toResponse(any(Transacao.class));
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma lista vazia ao consultar todos e não houver transações")
+    void deveRetornarListaVaziaAoConsultarTodos(){
+
+        when(repository.findAll()).thenReturn(Collections.emptyList());
+
+        List<PagamentoResponse> resultados = pagamentoService.consultarTodos();
+
+        assertNotNull(resultados);
+        assertEquals(0, resultados.size());
+
+        verify(repository).findAll();
         verify(mapper, never()).toResponse(any(Transacao.class));
     }
 }
