95d9774efd0bed30000d2d953414ee89cb3e17bc
Suzan Stockey Pereira <suzan-sp@hotmail.com>
2025-11-01 16:01:34 -0300

    feat(docs): configure API info using @OpenAPIDefinition

--- src/main/java/io/github/suzanstockey/toolschallenge/ToolsChallengeApplication.java
+++ src/main/java/io/github/suzanstockey/toolschallenge/ToolsChallengeApplication.java
@@ -1,8 +1,22 @@
 package io.github.suzanstockey.toolschallenge;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Tools Challenge - API de Pagamentos",
+                version = "1.0.0",
+                description = "API REST para simulação de transações de pagamento, estorno e consulta, como solução para o Tools Java Challenge [C].",
+                contact = @Contact(
+                        name = "Suzan Stockey Pereira",
+                        url = "https://github.com/SuzanStockey"
+                )
+        )
+)
 @SpringBootApplication
 public class ToolsChallengeApplication {
 
